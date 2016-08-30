1dd11e5033edd9ed09a247dbd8b0fdf90ac1e2b0
vsapucaia <vsfortunato@gmail.com>
2016-08-29 23:42:36 -0300

    mimic OK

--- basic/mimic.py
+++ basic/mimic.py
@@ -46,26 +46,39 @@ import sys
 
 
 def mimic_dict(filename):
-  """Returns mimic dict mapping each word to list of words which follow it."""
-  # +++your code here+++
-  return
+    dct = dict()
+
+    with open(filename, "r") as f:
+        text = f.read().lower()
+    words = text.split()
+
+    for i, word in enumerate(words):
+        if word not in dct.keys():
+            dct[word] = []
+        if i + 1 < len(words):
+            dct[word].append(words[i+1])
+
+    return dct
 
 
-def print_mimic(mimic_dict, word):
-  """Given mimic dict and start word, prints 200 random words."""
-  # +++your code here+++
-  return
+def print_mimic(mimic_dict, word, sentence_len):
+    """Given mimic dict and start word, prints 200 random words."""
+    text = [word]
+    for _ in range(sentence_len):
+        word = random.choice(mimic_dict[word])
+        text.append(word)
 
+    print(" ".join(text))
 
 # Provided main(), calls mimic_dict() and mimic()
 def main():
-  if len(sys.argv) != 2:
-    print('usage: ./mimic.py file-to-read')
-    sys.exit(1)
+    if len(sys.argv) != 2:
+        print('usage: ./mimic.py file-to-read')
+        sys.exit(1)
 
-  dict = mimic_dict(sys.argv[1])
-  print_mimic(dict, '')
+    dict = mimic_dict(sys.argv[1])
+    print_mimic(dict, 'mouse', 200)
 
 
 if __name__ == '__main__':
-  main()
+    main()
